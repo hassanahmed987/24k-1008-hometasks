4f889ac8855725087d4582302e56b4261d29bd7e
Hassan Ahmed <hassanahmedsiddqui@gmail.com>
2025-09-04 00:31:31 +0500

    made class and constructor

--- lab.01/task1.cpp
+++ lab.01/task1.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+#include <cstring>
 using namespace std;
 
+class ResearchPaper {
+private:
+    string* authors;   
+    int authorCount;
+
+public:
+  
+    ResearchPaper(int count, const string* names) {
+        authorCount = count;
+        authors = new string[authorCount];
+        for (int i = 0; i < authorCount; i++) {
+            authors[i] = names[i];
+        }
+    }
+};
 int main(){
-    cout<<"yeah";
+    cout<<"yeah"<<endl;
 }
