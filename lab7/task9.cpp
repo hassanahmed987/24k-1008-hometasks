900e6076e306012d8e3883f3f1cc4d9e5039186b
hassanahmed987 <hassanahmedsiddqui@gmail.com>
2025-10-31 15:07:22 +0500

    Create task9.cpp

---
+++ lab7/task9.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int getMax(int arr[], int n) {
+    int mx = arr[0];
+    for (int i = 1; i < n; i++)
+        if (arr[i] > mx)
+            mx = arr[i];
+    return mx;
+}
+
+void countSort(int arr[], int n, int exp) {
+    int output[n];
+    int count[10] = {0};
+
+    for (int i = 0; i < n; i++)
+        count[(arr[i] / exp) % 10]++;
+
+    for (int i = 1; i < 10; i++)
+        count[i] += count[i - 1];
+
+    for (int i = n - 1; i >= 0; i--) {
+        int idx = (arr[i] / exp) % 10;
+        output[count[idx] - 1] = arr[i];
+        count[idx]--;
+    }
+
+    for (int i = 0; i < n; i++)
+        arr[i] = output[i];
+}
+
+void radixSort(int arr[], int n) {
+    int m = getMax(arr, n);
+    for (int exp = 1; m / exp > 0; exp *= 10)
+        countSort(arr, n, exp);
+}
+
+int main() {
+    int n;
+    cout << "Enter number of transactions: ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter transaction amounts:\n";
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    radixSort(arr, n);
+
+    cout << "\nSorted transaction amounts (Ascending):\n";
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+
+    cout << endl;
+    return 0;
+}
